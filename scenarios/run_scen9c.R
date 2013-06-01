fe4d6601ec52ab6956155df46625645134dad8c6
ronkeizer <ronkeizer@gmail.com>
2013-05-31 21:11:52 -0400

    added scenarios

---
+++ scenarios/run_scen9c.R
@@ -0,0 +1,38 @@
+## implement the power analysis
+## Scenario 8: Enrollment slower in two arms - increments of 6 month (6 month, 1 year, 1.5 year and 2 years)
+
+source ("../cts_functions.R")   # the main simulation engine
+source ("../power_functions.R") # the functions for the statistical test and power analysis
+source ("../plot_functions.R")  # plotting & summarizing functions
+
+# registerDoMC(1)
+
+pat1 <- tte_patient_design (arm_start = 1)
+pat2 <- tte_patient_design (arm_start = 2)
+pat3 <- tte_patient_design (arm_start = 3) 
+pat4 <- tte_patient_design (arm_start = 4) 
+
+## Create an object describing the enrollment
+enrollment_design <- tte_enrollment_design ( 
+  enrollment_rate  = c(3000/(365*.5), 
+                       3000/(365*2), 
+                       3000/(365*2), 
+                       3000/(365*.5)   # enrollment rates in all arms (per day); all patients enrolled in 6 months
+)
+
+arm_design <- list (
+  "control" = tte_arm_design (hazard_event = 0.035, hazard_dropout = 0.1/1.5, hazard_switch = 0.1/1.5, n_patients=3000, patient_design = pat1),
+  "hc1" = tte_arm_design (hazard_event = 0.0525, hazard_dropout = 0.1/1.5, hazard_switch = 0.1/1.5, n_patients=3000, patient_design = pat2),
+  "hc2" = tte_arm_design (hazard_event = 0.0525, hazard_dropout = 0.1/1.5, hazard_switch = 0.1/1.5, n_patients=3000, patient_design = pat3),
+  "hc3" = tte_arm_design (hazard_event = 0.0525, hazard_dropout = 0.1/1.5, hazard_switch = 0.1/1.5, n_patients=3000, patient_design = pat4)
+)
+
+trial_design <- tte_trial_design (
+  arm_design = arm_design,
+  control_arm = 1,                                # which arm is the control
+  enrollment_design = enrollment_design,
+  visits = c(0, 90, 180, 270, 360, 450, 540),      # visit times (days)
+  max_events = NULL  # stopping criterion, can be implemented later as well
+)
+
+tte_run_power_analysis (trial_design, n_sim=150, max_events=572, name="scen9c")
